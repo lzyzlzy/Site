b953e65eb46d795d089f424d194bb1eba03e6e7b
liuzhenyu <liuzhenyu@ssit.cc>
2024-11-11 17:51:03 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App flex">
-      <div className='basis-1/5'>
-        <h1 className="text-3xl font-bold underline">
-          Zhenyu.Liu
-        </h1>
-        <p className='hover:bg-sky-500'>
-          Software Engineer
-        </p>
-        <p>
-          Street Photographer
-        </p>
+    <div className="App box-border" >
+      <div className='box-content h-full w-full border-4 border-white flex'>
+        <div className='basis-1/5'>
+          <h1 className="text-3xl font-bold underline">
+            Zhenyu.Liu
+          </h1>
+          <p className='hover:bg-sky-500'>
+            Software Engineer
+          </p>
+          <p>
+            Street Photographer
+          </p>
+          <p className='hover:font-bold underline'>
+            <a href='https://github.com/lzyzlzy'>Github</a>
+          </p>
+          <p className='hover:font-bold underline'>
+            <a href='https://www.instagram.com/andrewliu30/'>Instagram</a>
+          </p>
+        </div>
+        <div className='basis-4/5'>
+          <h1>Ok</h1>
+        </div>
       </div>
-      <div className='basis-4/5'>
-        <h1>Ok</h1>
-       </div>
     </div>
+
   );
 }
 
